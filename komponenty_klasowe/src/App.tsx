9655bc9fe8dfe469b1b8426daf85e5c4f7e61d14
PerzotProgrammer <bartoszperz2007@gmail.com>
2025-10-02 14:48:45 +0200

    Przesłano testowy projekt z lekcji

---
+++ komponenty_klasowe/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {ClassComponent} from "./ClassComponent.tsx";
+
+export default function App() {
+
+    return (
+        <>
+            <ClassComponent title={"This is class component1"}></ClassComponent>
+            <ClassComponent title={"This is class component2"}></ClassComponent>
+            <ClassComponent title={"This is class component3"}></ClassComponent>
+            <ClassComponent title={"This is class component4"}></ClassComponent>
+        </>
+    )
+}
+
